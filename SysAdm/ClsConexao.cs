765a5bfe77e1520e3eed407f15b76ed6e8f983c6
Gustavo Henrique <gustavohps58@gmail.com>
2020-02-26 14:30:56 -0300

    Create the ClsConexao class

---
+++ SysAdm/ClsConexao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SysAdm
+{
+    class ClsConexao
+    {
+        public SqlConnection conexao = new SqlConnection();
+
+        public SqlConnection conectar()
+        {
+            desconectar();
+            conexao.ConnectionString = "Server = IBM-PC\\SQLEXPRESS; Database = db_Tind4S; Uid = userTind4s; Trusted_Connection=True;";
+            //conexao.ConnectionString = "Server = db-tind4s.chwaokuky6hn.sa-east-1.rds.amazonaws.com, 1433; Database = db_Tind4S; Uid = adminTind4S; Pwd = mineirofoda;";
+            conexao.Open();
+            return conexao;
+        }
+
+        public void desconectar()
+        {
+            conexao.Close();
+        }
+    }
+}
